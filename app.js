8db8048e2aa806a1000705d536c0fc5db2a2f300
bbaxa16 <brooke769@gmail.com>
2017-07-18 09:37:03 -0500

    intitial setup

---
+++ app.js
@@ -0,0 +1,32 @@
+//Pseudo Code for Pokémon Project 1 - rough draft
+
+//home page
+
+//one DOM feature - the start button towards the bottom of the page, centered.
+//when start button is clicked (deciding if it should ge a button or a link to a new page.)
+
+//Go to the choose your adventurer
+//if we decide to use jquery:
+//remove home pages poke images
+//add the pick your adventure header
+//create three different adventurer options - 3 different characters.
+//the characters will be represented by an image and specs div, housed inside a div that will have an click event listener.
+
+//if selected that adventurer will be used from now on.
+//be a function that takes the adventures name as a param?
+
+//after picking your adventurer you'll pick your pokemon
+
+
+
+
+
+
+
+
+
+
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+//The picking and choosing can just be extra to start, number one priority is functionality. So to start we could just start with the click button going right into a battle.
+
+//battle will have our adventure and pokemon on the right and a wild pokemon (opponent) on the other side. they will battle with different attack options, will use similar logic with accuracy in battleship homework with accuracy being determined by math.random. first pokemon to lose all health points will faint and the other pokemon will win.
