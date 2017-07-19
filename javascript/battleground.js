990cdcdc3aef1512ffbb2163556de9f199bc0af3
bbaxa16 <brooke769@gmail.com>
2017-07-19 10:32:27 -0500

    started messing with jquery, added a ready to battle button.

--- javascript/battleground.js
+++ javascript/battleground.js
@@ -1,5 +1,9 @@
 $(()=>{ //window onload
 
+$('#ready').on('click', function(){
+  pikachu.getAccuracy();
+  pikachu.attack(snortle);
+} )
 
 })
 
@@ -10,14 +14,39 @@ class Pokemon {
     this.xp = 5;
     this.poketype = poketype;
     this.accuracy = '';
-    this.health = 100;
+    this.hp = 100;
+    this.coin = 300;
   }
   getAccuracy(){ //this will give all pokes a randomly generated accuracy between .5 and .10.
-    this.accuracy = Math.random() * (.5 - .10) + .5;
+    this.accuracy = Math.random() * (.7 - .4) + .4;
+    return this.accuracy;
   }
-}
+  attack(wildPoke){
+    if(this.poketype === "water"){}
+    else if (this.poketype === "grass"){}
+    else if (this.poketype === "fire"){}
+    else if (this.poketype === "electric"){
+      if(Math.random() < this.accuracy){
+        wildPoke.hp -= 22;
+        console.log('lightning strike was v effective, ' + wildPoke.name + ' loses 22 hp');
+        if(wildPoke.hp <= 0){ //need to add another function that will end this battle.
+          console.log('lightning strike was very effective. ' + wildPoke.name + ' fainted.');
+          alert('You have won this battle');
+          this.coin +=325;
+        }
+      }
+      else {
+        console.log('ya missed fool');
+      }
+    }
+
+    }
+  }
+
 
 const pikachu = new Pokemon("pikachu", 1, "electric");
 
-pikachu.getAccuracy();
-console.log(pikachu);
+const snortle = new Pokemon("snortle", 1, "grass");
+// pikachu.getAccuracy();
+// pikachu.attack(snortle);
+// pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);
