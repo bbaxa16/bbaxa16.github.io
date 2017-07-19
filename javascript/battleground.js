8d22d911de21dd1f8b66a80cf1a878671d414647
bbaxa16 <brooke769@gmail.com>
2017-07-19 08:11:34 -0500

    created pokemon class, created getAccuracy method and tested with a succesfully created pokemon

--- javascript/battleground.js
+++ javascript/battleground.js
@@ -1,5 +1,26 @@
 $(()=>{ //window onload
 
 
-
 })
+
+class Pokemon {
+  constructor (name, level, poketype){
+    this.name = name;
+    this.level = level;
+    this.xp = 5;
+    this.poketype = poketype;
+    this.accuracy = '';
+    this.health = 100;
+  }
+  getAccuracy(){ //this will give all pokes a randomly generated accuracy between the number 5 and 10.
+    Math.ceil(5);
+    Math.floor(10);
+    this.accuracy = Math.floor(Math.random()* (10-5)) + 5;
+    console.log(this.accuracy);
+  }
+}
+
+const pikachu = new Pokemon("pikachu", 1, "electric");
+
+pikachu.getAccuracy();
+console.log(pikachu);
