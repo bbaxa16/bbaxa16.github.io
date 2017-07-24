fa6c1936f6816dfe3023e08e6e450cd62c2cd4eb
bbaxa16 <brooke769@gmail.com>
2017-07-24 05:32:08 -0600

    styled our buttons

--- javascript/battleground.js
+++ javascript/battleground.js
@@ -3,7 +3,7 @@ $(()=>{ //window onload
 //global variables
 let ourPoke = " ";
 let wildPoke = " ";
-let $attackButton = $('<button>A T T A C K</button>')
+let $attackButton = $('<button>A T T A C K</button>').attr('id','attack').addClass('animation')
 
   class Pokemon { //creating a pokemon class
     constructor (name, level, poketype, img){
@@ -299,102 +299,28 @@ const createModal = (message, button1, button2) => {
 //The very first button thats pressed on this page
 $('#ready').on('click', function(){
   //selects our pokemon we will battle with
-  ourPoke = chooseOurPoke();
+  ourPoke = chooseOurPoke()
   //removes the 'ready to battle' button
-  $('#ready').remove();
+  $('#ready').remove()
   //puts our Poke on the battlefield
-  $('#ourPoke').append(ourPoke.img);
+  $('#ourPoke').append(ourPoke.img)
   //tells us who are pokemon is going to be
   createModal('Your pokémon is ' + ourPoke.name)
   // creates and attachs our battle button, when clicked will run the start().
-  const $battleButton = $('<button>Battle!</button>').attr('id', 'battle');
-  $('.container').append($battleButton);
+  const $battleButton = $('<button>Battle!</button>').attr('id', 'battle')
+  // $($battleButton).css('padding','10px 50px')
+  // $($battleButton).css('display','inline-block')
+  // $($battleButton).css('margin-left', '33%')
+  // $($battleButton).css('background-color','blue')
+  // $($battleButton).css('color','white')
+  // $($battleButton).css('font-family','monospace')
+  // $($battleButton).css('font-size','larger')
+  // $($battleButton).css('border','1px solid #241d13')
+  $('.container').append($battleButton)
   $($battleButton).on('click', function(){
     game.start();
 
   })
 })
-//****************************none of the stuff below matters *****************
-//
-// const pickUrPoke = () => { //usually called on '#ready' but taking it out for now
-//   //removes the ready for battle button
-//   $('#ready').remove();
-//   //removes the pokemon photo
-//   $('.default').remove();
-//   //adds the pick your Pokemon header
-//   const $pickYourPoke = $('<h1>Pick Your Pokémon</h2>');
-//   $('#header').append($pickYourPoke);
-//   //creates 3 new divs that will house the pokemon choices.
-//   const pokeChoice1 = $('<div/>').addClass('pokeChoice');
-//   //$(pokeChoice1).attr('id', 'pokeChoice1'); why wont it give it an id
-//
-//   const pokeChoice2 = $('<div/>').addClass('pokeChoice');
-//   const pokeChoice3 = $('<div/>').addClass('pokeChoice');
-//   $('.container').append(pokeChoice1);
-//   $('.container').append(pokeChoice2);
-//   $('.container').append(pokeChoice3);
-//   //adds pokenames to the divs.
-//   const $headerCharmander = $('<h2>Charmander</h2>');
-//   const $headerFroakie = $('<h2>Froakie</h2>');
-//   const $headerBellsprout = $('<h2>Bellsprout</h2>');
-//   $(pokeChoice1).append($headerCharmander);
-//   $(pokeChoice2).append($headerFroakie);
-//   $(pokeChoice3).append($headerBellsprout);
-//   //adds images of the pokemon choices to their respectice divs.
-//   const $imgCharmander = $('<img src="../img/charmander.png">').addClass('pokeImages');
-//   const $imgFroakie = $('<img src="../img/froakie.png">').addClass('pokeImages');
-//   const $imgBellsprout = $('<img src="../img/bellsprout.png">').addClass('pokeImages');
-//   $(pokeChoice1).append($imgCharmander);
-//   $(pokeChoice2).append($imgFroakie);
-//   $(pokeChoice3).append($imgBellsprout);
-//   //adds specs of the pokemon choices
-//   const $specsCharmander = $('<ul><li>Pokétype: fire</li></ul><p>Obviously prefers hot places. When it rains, steam is said to spout from the tip of its tail.</p>');
-//   const $specsFroakie = $('<ul><li>Pokétype: water</li></ul><p>It protects its skin by covering its body in delicate bubbles. Beneath its happy-go-lucky air, it keeps a watchful eye on its surroundings.</p>')
-//   const $specsBellsprout = $('<ul><li>Pokétype: grass</li></ul><p>Prefers hot and humid places. It ensnares tiny insects with tis vines and devours them.</p>');
-//   $(pokeChoice1).append($specsCharmander);
-//   $(pokeChoice2).append($specsFroakie);
-//   $(pokeChoice3).append($specsBellsprout);
-//   //create 3 different event listeners for each pokemon choice.
-//   $(pokeChoice1).on('click', function() {
-//     $(pokeChoice1).off('click');
-//     alert('You picked Charmander! Prepare for Battle!');
-//     $('#header').children().eq(1).remove();
-//     const $battleButton = $('<button>Battle!</button>').attr('id', 'battle');
-//     $('.container').append($battleButton);
-//     $(pokeChoice2).remove();
-//     $(pokeChoice3).remove();
-//     $($battleButton).on('click', function(){
-//       game.start();
-//     })
-//     //createModal(Charmander);
-//     //trying to link the charmander object to this div.
-//     //$(pokeChoice1).data(charmander);
-//     //$('.container').remove();
-//     // $(pokeChoice2).remove();
-//     // $(pokeChoice3).remove();
-//     // $(pokeChoice1).css('display:', 'center');
-//     //start();
-//
-//   });
-//   $(pokeChoice2).on('click', function(){
-//     alert('You picked Froakie!');
-//   });
-//   $(pokeChoice3).on('click', function(){
-//     alert('You picked Bellsprout!');
-//   });
-// }
-
-//createModal function
-// const createModal = (name) => {
-//   $('.container').remove();
-//   name.innerText = name
-//   $('#user-modal').append(name);
-
-  // $('#user-modal').css('display','block');
-  // $('#user-modal').append('<iframe src="https://giphy.com/embed/YzABsnRu6dM1a" width="480" height="382" frameBorder="0" allowFullScreen></iframe>').attr('id', 'charGif');
-
 
 }) //close of window onload
-
-
-// pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);pikachu.attack(snortle);
