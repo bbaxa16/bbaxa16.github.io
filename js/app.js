8642147837c58a817f2278ebaa787998a99eb090
bbaxa16 <brooke769@gmail.com>
2017-08-20 05:30:47 -0500

    switched to bootstrap

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const app = angular.module('myApp', []);
 
-app.controller('navCtrl', [function(){
+app.controller('mainCtrl', [function(){
   this.showNav = false;
   this.src = 'https://www.newfangled.com/wp-content/uploads/2014/08/stuffcontentmgrfiles2ac67e44c30a21635f8a9d498f832bc1cmisc_resized80_313_257_hamenu.png';
   this.clickNav = function(){
