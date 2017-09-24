4cc420c95ec5a7f293b24eca6b1bd164b822b66e
bbaxa16 <brooke769@gmail.com>
2017-09-24 16:27:50 -0500

    made nav have resp icons/update about section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const app = angular.module('baxaPortfolio', []);
 
-app.controller('ProjectCtrl', [function(){
+app.controller('projectCtrl', [function(){
+  this.message = poop;
   this.modal = false;
   this.toggleModal = function(){
     this.modal = !this.modal;
